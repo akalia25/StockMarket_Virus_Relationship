b9b9c70da2db61cf784cb8b279af3d08267d9b96
akalia25 <32119652+akalia25@users.noreply.github.com>
2020-01-31 02:21:54 -0500

    updated to have corona virus data

--- Market_Relationship.py
+++ Market_Relationship.py
@@ -13,15 +13,29 @@ import yfinance as yf
 def stock_market_data:
     iShares_China_ETF = yf.Ticker('MCHI')
     iShares_China_ETF_DF = iShares_China_ETF.history(period='1mo')
-    iShares_China_ETF_DF = iShares_China_ETF_DF.loc[:,['Close']]
-    iShares_China_ETF_DF = iShares_China_ETF_DF.rename(columns={'Close':'MCHI-CLOSE'})
+    iShares_China_ETF_DF = iShares_China_ETF_DF.loc[:, ['Close']]
+    iShares_China_ETF_DF = iShares_China_ETF_DF.rename(columns={'Close':
+                                                                'MCHI-CLOSE'})
 
     SPY = yf.Ticker('SPY')
     SPY_DF = SPY.history(period='1mo')
-    SPY_DF.loc[:,'StockName'] = 'SPY'
+    SPY_DF.loc[:, 'StockName'] = 'SPY'
     SPY_DF = SPY_DF[['Close']]
-    SPY_DF = SPY_DF.rename(columns={'Close':'SPY-CLOSE'})
+    SPY_DF = SPY_DF.rename(columns={'Close': 'SPY-CLOSE'})
     frames = [SPY_DF, iShares_China_ETF_DF]
     stock_market_df = pd.concat(frames, axis=1)
 
-    normalized_market = stock_market_df.iloc[0]/stock_market_df
+    normalized_market = stock_market_df/stock_market_df.iloc[0]
+
+    return normalized_market
+
+def virus_data:
+    # Data Obtained from https://www.worldometers.info/coronavirus/
+    virus_data = {'2020-01-21': 446, '2020-01-22': 579, '2020-01-23': 844,
+            '2020-01-24': 1312, '2020-01-25': 2015, '2020-01-26': 2801,
+            '2020-01-27': 4579, '2020-01-28': 6061, '2020-01-29': 7816,
+            '2020-01-30': 9821}
+
+    virus_DF = pd.DataFrame(virus_data.values(), index=virus_data.keys(),
+                            columns=['number_of_cases'])
+
