353c0e047a19442ca35df95b70c2c9b3b3c15307
akalia25 <32119652+akalia25@users.noreply.github.com>
2020-01-31 02:44:07 -0500

    Date Alignment+Plotting
    added relvant dates only  plus created basic plotting

--- Market_Relationship.py
+++ Market_Relationship.py
@@ -8,34 +8,53 @@ Created on Thu Jan 30 23:10:46 2020
 
 import pandas as pd
 import yfinance as yf
+from datetime import datetime
 
-
-def stock_market_data:
+def stock_market_data():
     iShares_China_ETF = yf.Ticker('MCHI')
-    iShares_China_ETF_DF = iShares_China_ETF.history(period='1mo')
-    iShares_China_ETF_DF = iShares_China_ETF_DF.loc[:, ['Close']]
-    iShares_China_ETF_DF = iShares_China_ETF_DF.rename(columns={'Close':
+    iShares_China_ETF_df = iShares_China_ETF.history(period='1mo')
+    iShares_China_ETF_df = iShares_China_ETF_df.loc[:, ['Close']]
+    iShares_China_ETF_df = iShares_China_ETF_df.rename(columns={'Close':
                                                                 'MCHI-CLOSE'})
+    iShares_China_ETF_df = iShares_China_ETF_df.loc[iShares_China_ETF_df.index
+                                                    >= '2020-01-21']
 
     SPY = yf.Ticker('SPY')
-    SPY_DF = SPY.history(period='1mo')
-    SPY_DF.loc[:, 'StockName'] = 'SPY'
-    SPY_DF = SPY_DF[['Close']]
-    SPY_DF = SPY_DF.rename(columns={'Close': 'SPY-CLOSE'})
-    frames = [SPY_DF, iShares_China_ETF_DF]
+    SPY_df = SPY.history(period='1mo')
+    SPY_df.loc[:, 'StockName'] = 'SPY'
+    SPY_df = SPY_df[['Close']]
+    SPY_df = SPY_df.rename(columns={'Close': 'SPY-CLOSE'})
+    SPY_df = SPY_df.loc[SPY_df.index >= '2020-01-21']
+    frames = [SPY_df, iShares_China_ETF_df]
     stock_market_df = pd.concat(frames, axis=1)
 
     normalized_market = stock_market_df/stock_market_df.iloc[0]
 
     return normalized_market
 
-def virus_data:
+def virus_data():
     # Data Obtained from https://www.worldometers.info/coronavirus/
     virus_data = {'2020-01-21': 446, '2020-01-22': 579, '2020-01-23': 844,
             '2020-01-24': 1312, '2020-01-25': 2015, '2020-01-26': 2801,
             '2020-01-27': 4579, '2020-01-28': 6061, '2020-01-29': 7816,
             '2020-01-30': 9821}
 
-    virus_DF = pd.DataFrame(virus_data.values(), index=virus_data.keys(),
+    date_index = virus_data.keys()
+    date_index = [datetime.strptime(x, '%Y-%m-%d') for x in date_index]
+    virus_df = pd.DataFrame(virus_data.values(), index=date_index,
                             columns=['number_of_cases'])
+    virus_df_norm = virus_df/virus_df.iloc[0]
 
+    return virus_df_norm
+
+
+def main():
+    market_df = stock_market_data()
+    virus_df = virus_data()
+    finaldf = pd.concat([market_df, virus_df], axis=1)
+    market_df.plot()
+    virus_df.plot()
+
+
+if __name__ == '__main__':
+    main()
